89f5f56f9115ff588fd383cb6a33e65fd952d98d
Brandon <student@codesmiths-Mac-mini.local>
2016-04-16 12:50:59 -0700

    Minor changes

--- client/factories/deckFactory.js
+++ client/factories/deckFactory.js
@@ -7,7 +7,7 @@ function DeckFactory($http) {
     deck: {id: 1, username: "Bob", title: "Cat Facts", description: "Fun things about felines"},
 //    deck: {},
     setDeck: function(deckId) {
-      // this.deck = $http.post('/decks', );
+      // this.deck = $http.post('/decks', { });
     },
     loadDeck: function() {
       return this.deck;
