75014fc196ac7ffc7925b04e9872dfc653c0465e
Masha Diminsky <mashadiminsky@Mashas-MacBook-Pro.local>
2016-04-18 09:41:29 -0700

    deleted unused comments

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -9,82 +9,76 @@ const connection = new Sequelize('potato', 'potato123', '123', {
 
 // card table
 var Cards = connection.define('cards', {
-  id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
-  deckId: Sequelize.INTEGER,
-  question: Sequelize.STRING,
-  answer: Sequelize.STRING,
-  numCorrect: Sequelize.INTEGER,
-  displayCount: Sequelize.INTEGER
+	id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+	deckId: Sequelize.INTEGER,
+	question: Sequelize.STRING,
+	answer: Sequelize.STRING,
+	numCorrect: Sequelize.INTEGER,
+	displayCount: Sequelize.INTEGER
 });
 
 // insert cards of a deck into postgres
 router.post('/create', function(req,res) {
-  connection.sync().then(function() {
-    Cards.create({
-      deckId: req.body.deckId,
-      question: req.body.question,
-      answer: req.body.answer,
-      numCorrect: 0,
-      displayCount: 0
-    }).then(function(newCard) {
-    res.send(newCard);
-  }).catch(function(error) {
-      console.error(error);
-    });
-  });
+	connection.sync().then(function() {
+		Cards.create({
+			deckId: req.body.deckId,
+			question: req.body.question,
+			answer: res.body.answer,
+			numCorrect: 0,
+			displayCount: 0
+	  }).catch(function(error) {
+		  console.error(error);
+		});
+	});
 
 });
-
-// INPUT GLORIOUS CARD CREATION: createCards(32, "What is the difference between a living person and a corpse?", "Time");
 
 // Alter successRate & displayCount as the user views the card
 // and when they get the correct answer
 router.post('/update', function(req,res) {
-  Cards.update({
-    numCorrect: req.body.numCorrect,
-    displayCount: req.body.displayCount
-  }, {
-    where: {
-      id: cardId
-    }
-  });
+	Cards.update({
+		numCorrect: req.body.numCorrect,
+		displayCount: req.body.displayCount
+	}, {
+		where: {
+			id: cardId
+		}
+	});
 });
-
-// INPUT: alterNumAndDisplayCounts(1, 50, 100);
 
 // user can edit their question and answer, changes will reflect in postgres
 router.post(function(req,res) {
-  Cards.update({
-    question: req.body.question,
-    answer: req.body.answer
-  }, {
-    where: {
-      id: req.body.id
-    }
-  });
+	Cards.update({
+		question: req.body.question,
+		answer: req.body.answer
+	}, {
+		where: {
+			id: req.body.id
+		}
+	});
 });
-// INPUT CHANGES: editRow(7, "Derpdie derp", "Sherp sherp!");
 
 // read all cards in 1 deck
 router.post( '/read', function(req, res) {
-  Cards.findAll({
-    where: {
-      deckId: req.body.deckId
-    }
-  }).then(function(cardsObj) {
-    res.send(cardsObj);
-  }).catch(function(error) {
-    console.error(error);
-  });
+	// console.log(req.body);
+	Cards.findAll({
+		where: {
+			deckId: req.body.deckId
+		}
+	}).then(function(decksObj) {
+		// res.send()
+	}).catch(function(error) {
+		console.error(error);
+	});
 });
 
 // delete card (row in cards)
 router.post(function(req,res) {
-  Cards.destroy({
-    where: {
-      id: req.body.id
-    }
-  });
+		Cards.destroy({
+			where: {
+				id: req.body.id
+			}
+		});
 });
 
 
