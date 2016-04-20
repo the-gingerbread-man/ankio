324ec14bc1610d8da669b9907ce4b92ad7471bc9
the-gingerbread-man <alex.patch3@gmail.com>
2016-04-20 15:23:55 -0700

    Cleaned up decks and cards

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -2,66 +2,46 @@ const express = require('express');
 const router = express.Router();
 const Card = require('./../db/dbController.js').Card;
 
-router.post('/create', function(req, res) {
-		  Card.create({
-			  deckId: req.body.deckId,
-			  question: req.body.question,
-			  answer: req.body.answer,
-			  numCorrect: 0,
-			  displayCount: 0,
-	  }).catch(function(error) {
-		    console.error(error);
-		});
-});
+router
+	.post('/create', (req, res) => {
+		  Card
+		  	.create({
+			  	deckId: req.body.deckId,
+			  	question: req.body.question,
+			  	answer: req.body.answer,
+			  	numCorrect: 0,
+			  	displayCount: 0,
+	  		})
+	  		.catch(function(error) {
+		    	console.error(error);
+				});
+	})
 
-// TODO: Chunk this
-// Alter successRate & displayCount as the user views the card
-// and when they get the correct answer
-router.post('/update', function(req, res) {
-	  Card.update({
-		  numCorrect: req.body.numCorrect,
-		  displayCount: req.body.displayCount
-		}, {
-		  where: {
-			  cardId: req.body.cardId
-		}
+	.post('/update', (req, res) => {
+	  Card
+	  	.update({ numCorrect: req.body.numCorrect, displayCount: req.body.displayCount },
+	  					{ where: { cardId: req.body.cardId }
+			});
+	})
+
+	.post( (req, res) => {
+	  Card
+	  	.update({ question: req.body.question, answer: req.body.answer },
+	  					{ where: { cardId: req.body.cardId }
+			});
+	})
+
+	.post('/read', function(req, res) {
+	  Card
+	  	.findAll({ where: { deckId: req.body.deckId }})
+	  	.then(res.send)
+	  	.catch(console.error);
+	})
+
+	.post(function(req, res) {
+		  Card
+		  	.destroy({ where: { cardI: req.body.cardId }});
 	});
-});
-
-// user can edit their question and answer, changes will reflect in postgres
-router.post(function(req, res) {
-	  Card.update({
-		  question: req.body.question,
-		  answer: req.body.answer
-	}, {
-		  where: {
-			  cardId: req.body.cardId
-		}
-	});
-});
-
-// read all Card in 1 deck
-router.post('/read', function(req, res) {
-	// console.log(req.body);
-	  Card.findAll({
-		  where: {
-			  deckId: req.body.deckId
-		}
-	}).then(function(decksObj) {
-		// res.send()
-	}).catch(function(error) {
-		  console.error(error);
-	});
-});
-
-// delete card (row in Card)
-router.post(function(req, res) {
-		  Card.destroy({
-			  where: {
-				  cardI: req.body.cardId
-			}
-		});
-});
 
 
 module.exports = router;
--- server/routes/decks.js
+++ server/routes/decks.js
@@ -1,49 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Deck = require('./../db/dbController.js').Deck;
+const Card = require('./../db/dbController.js').Card;
 
-// create a new deck, insert into postgres
-router.post('/create', function(req, res) {
+router
+	.post('/create', (req, res) => {
 		Deck
 		.create({ userId: req.body.userId, deckName: req.body.deckName })
 		.then(res.send)
 		.catch(console.error);
-});
-
-// delete a deck
-router.post('/destroy', function(req, res) {
-	  Deck.destroy({
-		  where: {
-			  deckId: req.body.deckId
-			}
-	});
-
-	  Cards.findAll({
-		  where: {
-			  deckId: req.body.deckId
-		}
-	}).then(function(cards) {
-		  cards.forEach(function(card) {
-			  Cards.destroy({
-				  where: {
-					  id: card.id
-				}
-			});
-		});
-	});
-});
-
-// read all Deck of 1 user
-router.get('/getAll', function(req, res) {
-	Deck.findAll({
-    where: {
-      userUserId: req.body.userId
-    }
-  }).then(function(DeckObj) {
-		res.send(DeckObj)
-	}).catch(function(error) {
-			console.error(error);
 	})
+
+	.post('/destroy', function(req, res) {
+	  Deck.destroy({ where: { deckId: req.body.deckId }});
+	  Cards
+	  	.findAll({
+		  	where: { deckId: req.body.deckId }
+		  })
+	  	.then(cards => {
+		  	cards.forEach(card => {
+			  	Card.destroy({ where: { id: card.id }});
+				});
+			});
+	})
+	
+	.get('/getAll', (req, res) => {
+		Deck
+			.findAll({ where: { userUserId: req.body.userId }})
+			.then(res.send)
+			.catch(console.error);
 });
 
 module.exports = router;
