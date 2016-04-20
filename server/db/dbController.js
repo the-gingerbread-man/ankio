d36efa28df0fd3fdf7502baa8ecee68ff98e7091
the-gingerbread-man <alex.patch3@gmail.com>
2016-04-19 21:03:33 -0700

    Converted to airbnb and linted dbController

--- server/db/dbController.js
+++ server/db/dbController.js
@@ -5,26 +5,26 @@ const sequelize = new Sequelize('ankio', 'ankio_user', 'ankio_password', {
 });
 
 const User = sequelize.define('user', {
-	userId: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
-	username: { type: Sequelize.STRING, unique: true, allowNull: false },
-	password: Sequelize.STRING,
+  userId: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
+  username: { type: Sequelize.STRING, unique: true, allowNull: false },
+  password: Sequelize.STRING,
 });
 
 const Deck = sequelize.define('deck', {
-	deckId: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
-	deckName: Sequelize.STRING,
-	created: { type: Sequelize.STRING, defaultValue: Date.now() },
-	updated: { type: Sequelize.STRING, defaultValue: Date.now() },
+  deckId: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
+  deckName: Sequelize.STRING,
+  created: { type: Sequelize.STRING, defaultValue: Date.now() },
+  updated: { type: Sequelize.STRING, defaultValue: Date.now() },
 });
 
 const Card = sequelize.define('card', {
-	cardId: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
-	question: Sequelize.STRING,
-	answer: Sequelize.STRING,
-	numCorrect: { type: Sequelize.INTEGER, defaultValue: 0 },
-	displayCount: { type: Sequelize.INTEGER, defaultValue: 0 },
-	created: { type: Sequelize.STRING, defaultValue: Date.now() },
-	updated: { type: Sequelize.STRING, defaultValue: Date.now() },
+  cardId: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
+  question: Sequelize.STRING,
+  answer: Sequelize.STRING,
+  numCorrect: { type: Sequelize.INTEGER, defaultValue: 0 },
+  displayCount: { type: Sequelize.INTEGER, defaultValue: 0 },
+  created: { type: Sequelize.STRING, defaultValue: Date.now() },
+  updated: { type: Sequelize.STRING, defaultValue: Date.now() },
 });
 
 User.hasMany(Deck);
@@ -32,4 +32,4 @@ Deck.hasMany(Card);
 
 sequelize.sync({ force: true });
 
-module.exports = { User, Deck, Card, };
+module.exports = { User, Deck, Card };
