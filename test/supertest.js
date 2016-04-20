f2b4af2fc8d3f2cd7cad091fd5656d44fa525bd5
the-gingerbread-man <alex.patch3@gmail.com>
2016-04-20 10:37:14 -0700

    Adding server tests

--- test/supertest.js
+++ test/supertest.js
@@ -1,101 +1,131 @@
-const app = require('./../server/server.js');
-const fs = require('fs');
-const path = require('path');
+require('./../server/server.js'); // Start the server
 const PORT = process.env.PORT || 3000;
 const HOST = `http://localhost:${PORT}`;
 const request = require('supertest')(HOST);
 const { expect, assert } = require('chai');
 
-describe('HTTP Server', function() {
-	describe('/', function() {
-		describe('GET', function() {
-			it('index.html', function(done) {
-				request
-					.get('/')
-					.expect(200)
-					.end((err, res) => {
-						console.log("Res.bdy: ", res.body);
-						done();
-					});
-					// .expect('Content-Type', /text\/html/, done);
-			});
-			it('no extraneous', function(done) {
-				request
-					.get('/not-a-route')
-					.expect(404)
-					.expect('Content-Type', /text\/html/, done);
-			});
-			it('app.js', function(done) {
-				request
-					.get('/app.js')
-					.expect(200)
-					.expect('Content-Type', /application\/javascript/, done);
-			});
-			it('styles.css', function(done) {
-				request
-					.get('/styles.css')
-					.expect(200)
-					.expect('Content-Type', /text\/css/, done);
-			});
+describe('HTTP Server', function () {
+  describe('/', function () {
+    describe('GET', function () {
+      it('index.html', function (done) {
+        request
+          .get('/')
+          .expect(200)
+          .end((err, res) => {
+            console.log('Res.bdy: ', res.body);
+            done();
+          });
+          // .expect('Content-Type', /text\/html/, done);
+      });
+      it('no extraneous', function (done) {
+        request
+          .get('/not-a-route')
+          .expect(404)
+          .expect('Content-Type', /text\/html/, done);
+      });
+      it('app.js', function (done) {
+        request
+          .get('/app.js')
+          .expect(200)
+          .expect('Content-Type', /application\/javascript/, done);
+      });
+      it('styles.css', function (done) {
+        request
+          .get('/styles.css')
+          .expect(200)
+          .expect('Content-Type', /text\/css/, done);
+      });
 
-			describe('Angular', function() {
-				it('loginController', function(done) {
-					request
-						.get('/controllers/loginController.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done);
-				});
-				it('createController', function(done) {
-					request
-						.get('/controllers/createController.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done);
-				});
-				it('contentController', function(done) {
-					request
-						.get('/controllers/contentController.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done);
-				});
-				it('mainController', function(done) {
-					request
-						.get('/controllers/mainController.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done);
-				});
-				it('testController', function(done) {
-					request
-						.get('/controllers/testController.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done);
-				});
-				it('deckFactory', function(done) {
-					request
-						.get('/factories/deckFactory.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done);
-				});
-				it('userFactory', function(done) {
-					request
-						.get('/factories/userFactory.js')
-						.expect(200)
-						.expect('Content-Type', /application\/javascript/, done)
-				});		
-			});
-		});
-	});
+      describe('Angular', function () {
+        it('loginController', function (done) {
+          request
+            .get('/controllers/loginController.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+        it('createController', function (done) {
+          request
+            .get('/controllers/createController.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+        it('contentController', function (done) {
+          request
+            .get('/controllers/contentController.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+        it('mainController', function (done) {
+          request
+            .get('/controllers/mainController.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+        it('testController', function (done) {
+          request
+            .get('/controllers/testController.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+        it('deckFactory', function (done) {
+          request
+            .get('/factories/deckFactory.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+        it('userFactory', function (done) {
+          request
+            .get('/factories/userFactory.js')
+            .expect(200)
+            .expect('Content-Type', /application\/javascript/, done);
+        });
+      });
+    });
+  });
 
-	describe('/users', function() {
-		it('Verifies User Auth', function(done) {
-			request
-				.get('/user')
-				.expect(200)
-				// .expect('Content-Type', /application\/json/, done);
-				.end(function(err, res) {
-					console.log("Res.body: ", res.body);
-					expect(res.headers['x-powered-by']).to.equal('Express');
-					done();
-				});
-		});
-	});
+  describe('users', function () {
+    const validAuth = { username: 'testuser', password: 'testing123' };
+    const invalidAuth = { username: 'testuser', password: 'incorrectPassword' };
+    
+    it('Creates New User', function (done) {
+      request
+        .post('/users/create')
+        .send(validAuth)
+        .expect(200)
+        .end(function (err, res) {
+          expect(err).to.not.exist;
+          done();
+        });
+    });
+    it('Accepts Correct Password', function (done) {
+      request
+        .post('/users/')
+        .send(validAuth)
+        .expect(200)
+        .end(function (err, res) {
+          expect(err).to.not.exist;
+          done();
+        });
+    });
+    it('Rejects Incorrect Password', function (done) {
+      request
+        .post('/users/')
+        .send(invalidAuth)
+        .expect(200)
+        .end(function (err, res) {
+          expect(err).to.not.exist;
+          done();
+        });
+    });
+    it('Does Not Create Duplicate Usernames', function (done) {
+      request
+        .post('/users/create/')
+        .send({ username: 'testuser', password: 'adifferentpassword' })
+        .expect(404)
+        .end(function (err, res) {
+          expect(err).to.exist;
+          done();
+        });
+    });
+  });
 });
